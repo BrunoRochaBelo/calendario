27c9f366ae8bf5ca47475c3e2bd4e2cd9e629862
rangelsilva <rangelsjc1@gmail.com>
2026-04-11 02:07:01 -0300

    v2.5.1: Remo\u00e7\u00e3o de bot\u00e3o redundante no cabe\u00e7alho e refinamento de layout

--- index.php
+++ index.php
@@ -540,11 +540,6 @@ foreach ($holidays as $mmdd => $hName) {
                     <?php endif; ?>
                     <span class="year-label"><?= $year ?></span>
                     
-                    <?php if (can('criar_eventos')): ?>
-                        <a href="novaatividade.php" class="hide-on-desktop btn-plus-header" title="Novo Evento">
-                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></svg>
-                        </a>
-                    <?php endif; ?>
                 </div>
 
                 <div class="nav-controls">
